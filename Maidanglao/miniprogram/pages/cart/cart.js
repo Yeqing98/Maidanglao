c4cb22a055681b93b3de7f550ec90b50d4c053cb
chenmenglong24 <2268234936@qq.com>
2019-06-02 17:06:39 +0800

    小程序之战

--- Maidanglao/miniprogram/pages/cart/cart.js
+++ Maidanglao/miniprogram/pages/cart/cart.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    zhushi: {imgUrl: '../../images/cart/zhushi.png', name: '双层汉堡', price: 20},
+    zhushi: {imgUrl: '../../images/cart/zhushi.png', name: '双层汉堡', price: 24.5},
     xiaoshi: [
       {imgUrl: '../../images/cart/xiaoshi1.png', name: '小食1', price: 10},
       {imgUrl: '../../images/cart/xiaoshi2.png', name: '小食2', price: 20},
@@ -18,14 +18,56 @@ Page({
       {imgUrl: '../../images/cart/drink2.png', name: '饮料2', price: 2},
       {imgUrl: '../../images/cart/drink3.png', name: '饮料3', price: 3},
       {imgUrl: '../../images/cart/drink4.png', name: '饮料4', price: 4},
-      {imgUrl: '../../images/cart/drink5.png', name: '饮料4', price: 5},
-      {imgUrl: '../../images/cart/drink6.png', name: '饮料5', price: 6},
-      {imgUrl: '../../images/cart/drink7.png', name: '饮料6', price: 7},
-      {imgUrl: '../../images/cart/drink8.png', name: '饮料7', price: 8},
+      {imgUrl: '../../images/cart/drink5.png', name: '饮料5', price: 5},
+      {imgUrl: '../../images/cart/drink6.png', name: '饮料6', price: 6},
+      {imgUrl: '../../images/cart/drink7.png', name: '饮料7', price: 7},
+      {imgUrl: '../../images/cart/drink8.png', name: '饮料8', price: 8},
     ],
-    price: 24.5,
-    number: 1,
+    xiaoshiPrice: 0,
+    drinkPrice: 0,
+    unitPrice: 0,
+    number: 0,
     totalPrice: 0
+  },
+  init(){
+    let Data = this.data;
+    let XiaoshiPrice = Data.xiaoshi[0].price;
+    let DrinkPrice = Data.drink[0].price;
+    this.setData({
+      xiaoshiPrice: XiaoshiPrice,
+      drinkPrice: DrinkPrice,
+    })
+    let UnitPrice = Data.zhushi.price + Data.xiaoshiPrice + Data.drinkPrice;
+    this.setData({
+      unitPrice: UnitPrice
+    })
+    // console.log(Data.xiaoshiPrice, Data.drinkPrice, Data.unitPrice);
+  },
+  chooseXiaoshi(e){
+    // console.log(e.detail.current);
+    let index = e.detail.current;
+    let Data = this.data;
+    let XiaoshiPrice = Data.xiaoshi[index].price;
+    this.setData({
+      xiaoshiPrice: XiaoshiPrice
+    })
+    let UnitPrice = Data.zhushi.price + Data.xiaoshiPrice + Data.drinkPrice;
+    this.setData({
+      unitPrice: UnitPrice
+    })
+  },
+  chooseDrink(e){
+    // console.log(e.detail.current);
+    let index = e.detail.current;
+    let Data = this.data;
+    let DrinkPrice = Data.drink[index].price;
+    this.setData({
+      drinkPrice: DrinkPrice
+    })
+    let UnitPrice = Data.zhushi.price + Data.xiaoshiPrice + Data.drinkPrice;
+    this.setData({
+      unitPrice: UnitPrice
+    })
   },
   clickAdd(){
     // console.log(e);
@@ -48,9 +90,18 @@ Page({
     this.getTotalPrice(subNumber)
   },
   getTotalPrice(num){
-    let TotalPrice = this.data.price * num;
+    let TotalPrice = this.data.unitPrice * num;
     this.setData({
       totalPrice: TotalPrice
+    })
+  },
+  navigate(){
+    wx.redirectTo({
+      url: '../example/example',
+      fail: wx.showToast({
+        title: '页面跳转失败',
+        icon: 'none'
+      })
     })
   },
 
@@ -59,7 +110,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.init();
   },
 
   /**
